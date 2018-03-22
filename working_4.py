2f92883b1c755c95cd08d7f086a0361475f455a0
HR-Testing <HR-Testing@infotecs-nt>
2018-03-22 20:11:21 +0300

    learning

---
+++ working_4.py
@@ -0,0 +1,14 @@
+mam_age = 89
+
+if mam_age > 50  :
+    print ("wow!")
+
+_list = [1, 2, 3, 8, 4, 5]
+_list.append(6)
+print (_list)
+
+_tuple = (1, 2, 3, 8, 4, 5)
+print (_tuple)
+
+_dict = {5: 1, 6: 2, 7: 3}
+print (_dict)
